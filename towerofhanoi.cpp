8e7a7cb191acedc1b157c556bd93fe387e3f344e
Divyakriti23 <53806768+Divyakriti23@users.noreply.github.com>
2020-04-24 17:17:23 +0530

    Update towerofhanoi.cpp

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-using namespace std;
-void tower(int n,char src,char temp,char dest);
-int main()
-{
-	int num;
-	cout<<"Enter the number of discs";
-	cin>>num;
-	tower(num,'A','B','C');
-	return 0;
-}
-void tower(int n,char src,char temp,char dest)
-{
-	if(n==1)
-	{
-		cout<<"move %d disc from %c to %c"<<n,src,dest<<endl;
-	}
-	else
-	{
-		tower(n-1,src,dest,temp);
-		cout<<"move %d disc from %c to %c"<<n,src,dest<<endl;
-		tower(n-1,temp,src,dest);
-	}
-}
-
+#include <iostream>
+using namespace std; 
+  
+void towerOfHanoi(int n, char from_rod, 
+                    char to_rod, char aux_rod)  
+{  
+    if (n == 1)  
+    {  
+        cout << "Move disk 1 from rod " << from_rod <<  
+                            " to rod " << to_rod<<endl;  
+        return;  
+    }  
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);  
+    cout << "Move disk " << n << " from rod " << from_rod << 
+                                " to rod " << to_rod << endl;  
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);  
+}  
+  
+// Driver code 
+int main()  
+{  
+    int n = 4; // Number of disks  
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods  
+    return 0;  
+} 
